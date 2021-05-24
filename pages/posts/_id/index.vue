1ca19ef141b694808f659dfc7ab3ec1a200288c2
fdellekart <florian.dellekart@hotmail.de>
2021-05-24 18:41:00 +0200

    26 Create main pages plus global style

--- pages/posts/_id/index.vue
+++ pages/posts/_id/index.vue
@@ -1,15 +1,69 @@
 <template>
-    <div class="single_post_page">
+    <div class="single-post-page container">
         <section class="post">
-            <h1>Title of the post</h1>
-            <div class="post_details">
-                <div>Last Updated on: XXX</div>
-                <div>Written By: NAME</div>
+            <h1 class="post-title">Title of the post</h1>
+            <div class="post-details">
+                <div class="post-detail">Last Updated on: XXX</div>
+                <div class="post-detail">Written By: NAME</div>
             </div>
             <p>Actual content of the post</p>
         </section>
-        <section class="post_feedback">
+        <section class="post-feedback">
             <p>Let me know what you think about my post per <a href="mailto:flo@blog.com">Mail</a></p>
         </section>
     </div>
 </template>
+
+<style scoped>
+    .single-post-page {
+    padding: 30px;
+    text-align: center;
+    box-sizing: border-box;
+    }
+
+    .post {
+    width: 100%;
+    }
+
+    @media (min-width: 768px) {
+    .post {
+        width: 600px;
+        margin: auto;
+    }
+    }
+
+    .post-title {
+    margin: 0;
+    }
+
+    .post-details {
+    padding: 10px;
+    box-sizing: border-box;
+    border-bottom: 3px solid #ccc;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    }
+
+    @media (min-width: 768px) {
+    .post-details {
+        flex-direction: row;
+    }
+    }
+
+    .post-detail {
+    color: rgb(88, 88, 88);
+    margin: 0 10px;
+    }
+
+    .post-feedback a {
+    color: red;
+    text-decoration: none;
+    }
+
+    .post-feedback a:hover,
+    .post-feedback a:active {
+    color: salmon;
+    }
+</style>
