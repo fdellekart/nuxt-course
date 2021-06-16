05a0f40154eb305ecbb64c7a9c98fe9c08334624
fdellekart <florian.dellekart@hotmail.de>
2021-06-16 19:31:59 +0200

    asyncdata for single post

--- pages/posts/_id/index.vue
+++ pages/posts/_id/index.vue
@@ -1,18 +1,39 @@
 <template>
     <div class="single-post-page container">
         <section class="post">
-            <h1 class="post-title">Title of the post</h1>
+            <h1 class="post-title">{{ post.title }}</h1>
             <div class="post-details">
-                <div class="post-detail">Last Updated on: XXX</div>
-                <div class="post-detail">Written By: NAME</div>
+                <div class="post-detail">Last Updated on: {{ post.lastUpdatedDate }}</div>
+                <div class="post-detail">Written By: {{ post.author }}</div>
             </div>
-            <p>Actual content of the post</p>
+            <p>{{ post.content }}</p>
         </section>
         <section class="post-feedback">
             <p>Let me know what you think about my post per <a href="mailto:flo@blog.com">Mail</a></p>
         </section>
     </div>
 </template>
+
+<script>
+export default {
+    asyncData(context, callback) {
+        setTimeout(() => {
+            callback(null, {
+                post: {
+                id: "1",
+                title: "A Post! (ID: " + context.params.id + ")",
+                previewText: "A guada!",
+                author: "Flo",
+                lastUpdatedDate: new Date(),
+                content: "Some content for the post",
+                thumbnail: "https://www.ikea.com/mx/en/images/products/pjaetteryd-picture-trolltunga-norway__0925582_pe788810_s5.jpg"
+            }
+            })
+        },
+        1000)
+    }
+}
+</script>
 
 <style scoped>
     .single-post-page {
--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -13,24 +13,27 @@ export default {
     components: {
         PostList
     },
-    data() {
-        return {
-        posts: [
+    asyncData(context, callback) {
+        console.log(context)
+        setTimeout(() => {
+        callback(null, {
+            posts: [
             {
-            id: 1,
+            id: "1",
             title: "A Post!",
             previewText: "A guada!",
             thumbnail: "https://www.ikea.com/mx/en/images/products/pjaetteryd-picture-trolltunga-norway__0925582_pe788810_s5.jpg"
             },
             {
-            id: 2,
+            id: "2",
             title: "A aundara Post!",
             previewText: "A Bessana!",
             thumbnail: "https://www.ikea.com/mx/en/images/products/pjaetteryd-picture-trolltunga-norway__0925582_pe788810_s5.jpg"
             }
         ]
-        }
-    }
+      }) 
+    }, 1500)
+  }
 }
 </script>
 
