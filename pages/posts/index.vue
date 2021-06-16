684041fe3a20ff6414e26acc652a92d13174d6f4
fdellekart <florian.dellekart@hotmail.de>
2021-06-16 20:38:34 +0200

    Move posts fetching to nuxt server init action in store

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -13,30 +13,6 @@ export default {
     components: {
         PostList
     },
-    fetch(context) {
-        return new Promise((resolve, reject) => {
-            setTimeout(() => {
-            resolve({
-                posts: [
-                {
-                id: "1",
-                title: "A Post!",
-                previewText: "A guada!",
-                thumbnail: "https://www.ikea.com/mx/en/images/products/pjaetteryd-picture-trolltunga-norway__0925582_pe788810_s5.jpg"
-                },
-                {
-                id: "2",
-                title: "A aundara Post!",
-                previewText: "A Bessana!",
-                thumbnail: "https://www.ikea.com/mx/en/images/products/pjaetteryd-picture-trolltunga-norway__0925582_pe788810_s5.jpg"
-            }
-        ]
-      }) 
-    }, 1500);
-        }).then(data => {
-            context.store.commit('setPosts', data.posts)
-        })
-  },
   computed: {
       loadedPosts() {
           return this.$store.getters.loadedPosts
--- store/index.js
+++ store/index.js
@@ -12,6 +12,26 @@ const createStore = () => {
                 } 
             },
             actions: {
+                nuxtServerInit(vuexContext, context) {
+                    return new Promise((resolve, reject) => { 
+                        vuexContext.commit('setPosts', 
+                        [
+                            {
+                            id: "1",
+                            title: "A Post!",
+                            previewText: "A guada!",
+                            thumbnail: "https://www.ikea.com/mx/en/images/products/pjaetteryd-picture-trolltunga-norway__0925582_pe788810_s5.jpg"
+                            },
+                            {
+                            id: "2",
+                            title: "A aundara Post!",
+                            previewText: "A Bessana!",
+                            thumbnail: "https://www.ikea.com/mx/en/images/products/pjaetteryd-picture-trolltunga-norway__0925582_pe788810_s5.jpg"
+                        }
+                        ])
+                        resolve() 
+                })
+                },
                 setPosts(vuexContext, posts) {
                     vuexContext.commit('setPosts', posts)
                 }
